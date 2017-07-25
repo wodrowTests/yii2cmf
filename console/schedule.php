d36a39d572585ef60027761ae2f38cc1e2963474
wodrow <wodrow451611cv@gmail.com>
2017-07-25 10:32:26 +0800

    update cron exec

--- console/schedule.php
+++ console/schedule.php
@@ -5,7 +5,9 @@
  * Date: 16/6/16
  * Time: 下午9:57
  */
-
+/**
+ * @var \omnilight\scheduling\Schedule $schedule
+ */
 /**
  * 任务调度
  * crontab -e * * * * * php /path/to/yii schedule/run  1>> /dev/null 2>&1
